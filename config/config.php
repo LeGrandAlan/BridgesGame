ab9b0fdfd494eb553002ddf787866857d4b27b83
TheERW85 <theerw85@live.fr>
2018-11-19 14:00:46 +0100

    debut

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("HOME_SITE",__DIR__."/..");
+
+define("PATH_VUE",HOME_SITE."/vue");
+define("PATH_CONTROLEUR",HOME_SITE."/controleur");
+define("PATH_MODELE",HOME_SITE."/modele");
+
+define("HOST","localhost");
+define("BD","E174590X");
+define("LOGIN","E174590X");
+define("PASSWORD","E174590X");
+?>
