8b38ee06bac9d8e356e95452dc2a666b712be8df
Alan Le Grand <pro.alan.le.grand@gmail.com>
2018-11-19 15:04:07 +0100

    Adding colors to indicate blocked or selected cities

--- vue/jeu.php
+++ vue/jeu.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
-$_SESSION['villes'][0][0]="3";
-$_SESSION['villes'][0][6]="2";
-$_SESSION['villes'][3][0]="6";
-$_SESSION['villes'][3][5]="2";
-$_SESSION['villes'][5][1]="1";
-$_SESSION['villes'][5][6]="2";
-$_SESSION['villes'][6][0]="2";
+//                         [nombrePontsMax, nombrePontsActuel, estSelectionne]
+$_SESSION['villes'][0][0]=[3, 0, 0];
+$_SESSION['villes'][0][6]=[2, 0, 0];
+$_SESSION['villes'][3][0]=[6, 0, 0];
+$_SESSION['villes'][3][5]=[2, 2, 0];
+$_SESSION['villes'][5][1]=[1, 0, 0];
+$_SESSION['villes'][5][6]=[2, 0, 1];
+$_SESSION['villes'][6][0]=[2, 0, 0];
 if(!isset($_SESSION['villes'])) {
     echo "erreur de grille de jeu"; //TODO: il faudra rediriger vers page d'erreur
 }
@@ -30,15 +31,29 @@ if(!isset($_SESSION['villes'])) {
         text-decoration: none;
         color: black;
     }
+    th a#selected {
+        background-color: green;
+    }
+    th a.blocked {
+        background-color: red;
+    }
 </style>
 <table>
 <?php
-
 for ($x = 0; $x < 7; $x++){
     echo "<tr>";
     for ($y = 0; $y < 7; $y++){
         if (isset($_SESSION['villes'][$x][$y])) {
-            echo "<th><a href='index.php?x=". $x ."&y=". $y ."'>" . $_SESSION['villes'][$x][$y] . "</a></th>";
+            if ($_SESSION['villes'][$x][$y][2]){
+                // Si la ville est selectionnée, on l'indique au joueur
+                echo "<th><a id='selected' href='index.php?x=". $x ."&y=". $y ."'>" . $_SESSION['villes'][$x][$y][0] . "</a></th>";
+            } else if($_SESSION['villes'][$x][$y][1]) {
+                // Si la ville a son max de ponts, on l'indique
+                echo "<th><a class='blocked' href='index.php?x=". $x ."&y=". $y ."'>" . $_SESSION['villes'][$x][$y][0] . "</a></th>";
+            } else {
+                // Si la ville n'est ni selectionnée, ni max de ponts, on l'affiche juste
+                echo "<th><a href='index.php?x=". $x ."&y=". $y ."'>" . $_SESSION['villes'][$x][$y][0] . "</a></th>";
+            }
         } else {
             echo "<th></th>";
         }
